8581572ed316b7d3cf1342aa9fbe16be0a27367a
Tea Huang <Teaghuang@gmail.com>
2023-06-26 12:43:56 -0400

    Initial Commit

---
+++ duckBoatProject/duckBoatProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  duckBoatProjectApp.swift
+//  duckBoatProject
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct duckBoatProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
